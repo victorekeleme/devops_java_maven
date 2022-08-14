3d1d41f3248e95cfaf9c97598f9a848ab32b2037
victorekeleme <ekelemevictor99@gmail.com>
2022-08-14 14:21:09 -0400

    updated jenkinsfile

--- script.groovy
+++ script.groovy
@@ -3,18 +3,6 @@ def TestApp(){
     sh "mvn test"
 }
 
-// def buildAppJar() {
-//     echo "Building the application jar"
-//     sh "mvn package"
-// }
-// def buildAppImage() {
-//     echo "Building the application docker image"
-//     withCredentials([usernamePassword(credentialsId: 'Docker-Repo-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//         sh "docker build -t vistein12/java-maven-app:${VERSION} ."
-//         sh "echo $PASS | docker login -u $USER --password-stdin"
-//         sh "docker push vistein12/java-maven-app:${VERSION}"
-//     }
-// }
 def deployApp() {
     echo "Deploying the application"
     
