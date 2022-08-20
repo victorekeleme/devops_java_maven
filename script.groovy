b1d4aa7f6d8e185df9af405cb89e5d20ea7a1319
victorekeleme <ekelemevictor99@gmail.com>
2022-08-19 22:41:42 -0400

    jenkinsfile update

--- script.groovy
+++ script.groovy
@@ -23,17 +23,18 @@ def pushAWS(String IMAGE_NAME){
     echo "Pushing Docker Image to AWS"
 
     // sh "aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 524360703326.dkr.ecr.us-east-2.amazonaws.com"
-    sh "aws configure"
-    sh "echo AKIAXUFSVLVPPRSEDEGO"
+    withCredentials([aws(accessKeyVariable:'AWS_ACCESS_KEY_ID',credentialsId:'aws-credentials',secretKeyVariable:'AWS_SECRET_ACCESS_KEY')]) {
+        sh "aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 524360703326.dkr.ecr.us-east-2.amazonaws.com"   
+        sh "docker tag $IMAGE_NAME 524360703326.dkr.ecr.us-east-2.amazonaws.com/$IMAGE_NAME"
+        sh "docker push 524360703326.dkr.ecr.us-east-2.amazonaws.com/$IMAGE_NAME"
+    }
 
     // 'http://524360703326.dkr.ecr.us-east-2.amazonaws.com',
     // 'ecr:us-east-2:aws-credentials'
     // sh "docker tag $IMAGE_NAME 524360703326.dkr.ecr.us-east-2.amazonaws.com/$IMAGE_NAME"
     // sh "docker push 524360703326.dkr.ecr.us-east-2.amazonaws.com/$IMAGE_NAME"
 
-    // sh "docker tag $IMAGE_NAME 524360703326.dkr.ecr.us-east-2.amazonaws.com/$IMAGE_NAME"
-    // sh "docker push 524360703326.dkr.ecr.us-east-2.amazonaws.com/$IMAGE_NAME"
-
+ 
 }
 
 
